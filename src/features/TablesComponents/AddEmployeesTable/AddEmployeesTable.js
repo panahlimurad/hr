1e40b8f5b58b7348b6dcc1badbefc9fec5d6ed9d
panahlimurad <panahli.murad@gmail.com>
2024-08-19 18:28:40 +0400

    pickers

--- src/features/TablesComponents/AddEmployeesTable/AddEmployeesTable.js
+++ src/features/TablesComponents/AddEmployeesTable/AddEmployeesTable.js
@@ -94,6 +94,7 @@ const AddEmployeesTable = () => {
                 placeholder="Address"
               />
               <BasicDatePicker label="Date of Joining" />
+              
               <select
                 id="underline_select"
                 className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
